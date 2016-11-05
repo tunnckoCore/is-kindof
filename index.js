039c371f92d97b8ed714b6e02e867b9720556a22
tunnckoCore <mameto_100@mail.bg>
2016-11-05 02:08:19 +0200

    style(*): update boilerplate stuff

--- index.js
+++ index.js
@@ -1,74 +1,8 @@
 /*!
  * is-kindof <https://github.com/tunnckoCore/is-kindof>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
 'use strict'
-
-var types = require('kind-of-types')
-var kindOf = require('kind-of-extra')
-
-function Is (val, types) {
-  if (arguments.length === 2) {
-    types = arrayify(types)
-
-    var ret = false
-    var len = types.length
-    var i = 0
-
-    while (i < len) {
-      var type = types[i++]
-      if (kindCheck(val, type)) {
-        ret = true
-        break
-      }
-    }
-    return ret
-  }
-  if (arguments.length <= 1) {
-    if (!(this instanceof Is)) {
-      return new Is(val)
-    }
-    this.marker = arguments
-    this.value = val
-  }
-  return false
-}
-
-function arrayify (val) {
-  return Array.isArray(val) ? val : [val]
-}
-
-function kindCheck (val, type) {
-  if (type === 'generatorFunction' || type === 'generatorfunction') {
-    return kindOf(val) === 'generatorfunction'
-  }
-
-  return kindOf(val) === type
-}
-
-types.forEach(function (type) {
-  var typeName = 'is' + type[0].toUpperCase() + type.slice(1)
-
-  function isType (val) {
-    if (arguments.length === 1) {
-      return kindCheck(val, type)
-    }
-    if (this.marker === undefined) {
-      return false
-    }
-    return kindCheck(this.value, type)
-  }
-
-  Is[type] = isType
-  Is[typeName] = isType
-  Is.prototype[type] = isType
-  Is.prototype[typeName] = isType
-})
-
-Is.generatorFunction = Is.generatorfunction
-Is.prototype.generatorFunction = Is.prototype.generatorfunction
-
-module.exports = Is
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * is-kindof <https://github.com/tunnckoCore/is-kindof>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
@@ -9,42 +9,8 @@
 
 'use strict'
 
-var test = require('assertit')
-var is = require('./index')
+var test = require('mukla')
 
-test('is-kindof:', function () {
-  test('should return true when work as `kind-of` sugar - `is(val, type)`', function (done) {
-    require('./test/kind-of-sugar')
-    done()
-  })
-  test('should return false when `is(val, type)`', function (done) {
-    test.equal(is(/regex/, 'string'), false)
-    test.equal(is({a: 'bcd'}, 'stream'), false)
-    test.equal(is([1, 2, 3], 'number'), false)
-    done()
-  })
-  test('should be able `types` to be array - `is(val, [type, type, type])`', function (done) {
-    require('./test/array-types-support')
-    done()
-  })
-  test('should have static methods for all types - `is.method`', function (done) {
-    require('./test/static-methods')
-    done()
-  })
-  test('should have prototype methods for all types - `is().method`', function (done) {
-    require('./test/prototype-methods')
-    done()
-  })
-  test('should return true for `is(val).method()', function (done) {
-    require('./test/ctor-value-method')
-    done()
-  })
-  test('should return true for `is().method(val)', function (done) {
-    require('./test/ctor-method-value')
-    done()
-  })
-  test('should return true for `is.method(val)', function (done) {
-    require('./test/static-method-value')
-    done()
-  })
+test(function (done) {
+  done()
 })
