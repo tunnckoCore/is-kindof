ef6a9d642ad963a8718a868b8e71bbbce8f7b1bb
tunnckoCore <mameto_100@mail.bg>
2016-11-05 02:39:23 +0200

    refactor(simplify): update boilerplate and simplify
    BREAKING CHANGE: exports and module.exports only methods for types - is.string(val),
    
    is.function(val), is.object(val) and etc. because before that it was too complex and needless. Now
    
    it is clean and explicit.

--- index.js
+++ index.js
@@ -6,3 +6,16 @@
  */
 
 'use strict'
+
+var utils = require('./utils')
+
+function check (type) {
+  return function is (val) {
+    return utils.typeOf(val) === type
+  }
+}
+
+utils.types.forEach(function each (type) {
+  exports[type] = check(type)
+  module.exports[type] = check(type)
+})
--- test.js
+++ test.js
@@ -10,7 +10,43 @@
 'use strict'
 
 var test = require('mukla')
+var is = require('./index')
+var through2 = require('through2')
 
-test(function (done) {
+var keys = Object.keys(is)
+
+var actual = {
+  'array': [1, 2],
+  'boolean': true,
+  'buffer': new Buffer('foo'),
+  'date': new Date(),
+  'error': new Error('foo'),
+  'function': function noop () {},
+  'generator': (function * () { yield 42 })(),
+  'generatorfunction': function * () {},
+  'map': new Map(),
+  'null': null,
+  'number': 123,
+  'object': { a: 'b' },
+  'promise': Promise.resolve(123),
+  'regexp': new RegExp('aa'),
+  'set': new Set(),
+  'stream': through2(),
+  'string': 'fooo',
+  'symbol': Symbol(),
+  'undefined': undefined,
+  'weakmap': new WeakMap(),
+  'weakset': new WeakSet()
+}
+
+test('should export >=22 methods', function (done) {
+  test.ok(keys.length > 21)
   done()
+})
+
+Object.keys(actual).forEach(function (name) {
+  test('is.' + name + '(val) work', function (done) {
+    test.ok(is[name](actual[name]), name + ' should work')
+    done()
+  }, true)
 })
