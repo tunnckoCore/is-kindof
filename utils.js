9c3af8d6e4bae30c727c7833bbbe3d56b670553e
tunnckoCore <mameto_100@mail.bg>
2016-11-05 02:14:06 +0200

    chore(yarn): update

---
+++ utils.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var utils = require('lazy-cache')(require)
+var fn = require
+require = utils // eslint-disable-line no-undef, no-native-reassign, no-global-assign
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('kind-of-extra', 'typeOf')
+require('kind-of-types', 'types')
+require = fn // eslint-disable-line no-undef, no-native-reassign, no-global-assign
+
+/**
+ * Expose `utils` modules
+ */
+
+module.exports = utils
